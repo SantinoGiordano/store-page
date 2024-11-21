f821a24250eb5374635ba82baff6426ae42ef378
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-21 09:52:03 -0500

    imported loading.tsx

--- app/componets/navigation.tsx
+++ app/componets/navigation.tsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 export default function Navagation(){
     const pathName = usePathname()
     return(
-        <nav>
+        <nav className="">
             <Link href='/' className={pathName === "/" ? "font-bold mr-4":"mr-4"}>Home</Link>
             <Link href='/about'className={pathName === "/about" ? "font-bold mr-4":"mr-4"}>About</Link>
             <Link href='/products/1' className={pathName === "/products/1" ? "font-bold mr-4":"mr-4"}>Products 1</Link>
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>THINKING</div>
+  )
+}
+
+export default loading
